9bd5c9135fa28d2e499a62d1b6c6d2078ebaa23b
lennylxx <lennylxx@gmail.com>
2014-10-23 15:31:39 +0800

    update scripts, add google photo domains

--- update_hosts.sh
+++ update_hosts.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+    echo -e "Usage:"
+    echo -e "    $ ./update_hosts.sh hosts new_hosts\n"
+    exit 1
+fi
+
 hosts_file=$1
 new_hosts_file=$2
+
+if [ ! -f "$hosts_file" ]; then
+    echo -e $1" doesn't exists, plz retry!\n"
+    exit 1
+fi
+
+if [ -f $new_hosts_file ]; then
+    new_hosts_file=${new_hosts_file}".new"
+    printf "" > $new_hosts_file
+fi
+
+touch $new_hosts_file
 
 he_net="2001:470:20::2"
 google_a="2001:4860:4860::8888"
@@ -29,52 +47,52 @@ num=1
 while read line
 do
 {
-	#delete CR
-	line=$(printf "$line"|tr -d '\r')
-	#printf "$line"|od -tx1
+    #delete CR
+    line=$(printf "$line"|tr -d '\r')
+    #printf "$line"|od -tx1
 
-	if [[ $line == "" ]]; then 
-		printf "\r\n" >> $new_hosts_file
-		continue
-	fi
+    if [[ $line == "" ]]; then 
+        printf "\r\n" >> $new_hosts_file
+        continue
+    fi
 
-	if [ "${line:0:2}" == "##" ]; then 
-		printf "$line\r\n" >> $new_hosts_file
-		continue
-	fi
-	
-	if [ "${line:0:1}" == "#" ]; then 
-		line=${line#'#'}
-	fi
-	
-	url=$(printf "$line"|cut -d" " -f2)
+    if [ "${line:0:2}" == "##" ]; then 
+        printf "$line\r\n" >> $new_hosts_file
+        continue
+    fi
+    
+    if [ "${line:0:1}" == "#" ]; then 
+        line=${line#'#'}
+    fi
+    
+    url=$(printf "$line"|cut -d" " -f2)
 
-	result=$(nslookup -querytype=AAAA "$url" "$dns"|grep 'AAAA address')
-	
-	name=$(printf "$result"|cut -f1)
-	ip=$(printf "$result"|cut -d' ' -f4)
-	
-	for var in "${blackhole[@]}"; do
-	if [[ $ip == "$var" && $ip != "" ]]; then
-		ip=$(nslookup -vc -querytype=AAAA "$url" "$dns"|grep 'AAAA address'|cut -d' ' -f4)
-		break
-	fi
-	done
-	
-	if [[ $ip == "" ]]; then
-		printf "#$line\r\n" >> $new_hosts_file
-		continue
-	fi
+    result=$(nslookup -querytype=AAAA "$url" "$dns"|grep 'AAAA address')
+    
+    name=$(printf "$result"|cut -f1)
+    ip=$(printf "$result"|cut -d' ' -f4)
+    
+    for var in "${blackhole[@]}"; do
+    if [[ $ip == "$var" && $ip != "" ]]; then
+        ip=$(nslookup -vc -querytype=AAAA "$url" "$dns"|grep 'AAAA address'|cut -d' ' -f4)
+        break
+    fi
+    done
+    
+    if [[ $ip == "" ]]; then
+        printf "#$line\r\n" >> $new_hosts_file
+        continue
+    fi
 
-	if [[ $name != $url && $name != "" ]]; then
-		url=${url}" #"${name}
-	fi
+    if [[ $name != $url && $name != "" ]]; then
+        url=${url}" #"${name}
+    fi
 
-	printf "$ip $url\r\n" >> $new_hosts_file
+    printf "$ip $url\r\n" >> $new_hosts_file
 
-	#print log to stdio
-	echo "$num" "$ip" "$url"
-	num=$((num+1))
+    #print log to stdio
+    echo "$num" "$ip" "$url"
+    num=$((num+1))
 }
 done < $hosts_file
 

ad90c035a9cd32bdbfbc1262aa163319a03bd75b
lennylxx <lennylxx@gmail.com>
2014-10-23 15:31:39 +0800

    update scripts, add google photo domains

--- merge_snippets.sh
+++ merge_snippets.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+    echo -e "Usage:"
+    echo -e "    $ ./merge_snippets.sh new_hosts\n"
+    exit 1
+fi
+
 new_hosts_file=$1
+
+if [ -f $new_hosts_file ]; then
+    new_hosts_file=${new_hosts_file}".new"
+    printf "" > $new_hosts_file
+fi
 
 date=$(date "+%m/%d/%Y")
 

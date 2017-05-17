bbc28354f95abd4d62e8229f8e49587bf080be34
lennylxx <lennylxx@gmail.com>
2017-05-17 22:53:12 +0800

    use spaces to indent instead of tabs

--- tools/list-sn-spec.py
+++ tools/list-sn-spec.py
@@ -5,7 +5,7 @@ import sys
 def main():
     sn = sys.argv[1]
     for k in range(1, 21):
-    	print 'r%d' % k + '---' + 'sn-' + sn + '.googlevideo.com'
+        print 'r%d' % k + '---' + 'sn-' + sn + '.googlevideo.com'
 
 if __name__ == '__main__':
     main()

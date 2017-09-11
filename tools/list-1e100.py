e7146364e9efd5f137b0111324de44c8bda2d7de
lennylxx <lennylxx@gmail.com>
2017-09-11 23:34:36 +0800

    Update 1e100.net enumerate script

--- tools/list-1e100.py
+++ tools/list-1e100.py
@@ -8,15 +8,21 @@ table = (
     'hkg', 'iad', 'kix', 'kul', 'lax', 'lga', 'lhr', 'lis',
     'maa', 'mad', 'mia', 'mil', 'mrs', 'muc', 'nrt', 'nuq',
     'ord', 'par', 'pek', 'prg', 'qro', 'sea', 'sha', 'sin',
-    'sjc', 'sof', 'syd', 'waw', 'yyz', 'zrh'
+    'sjc', 'sof', 'syd', 'tsa', 'waw', 'yyz', 'zrh'
 )
 
+def list_1e100(iata):
+    for i in range(1, 40):
+        for j in range(1, 90):
+            print iata + str(i).zfill(2) + 's' + str(j).zfill(2) + '-in-'\
+                  'x' + '01.1e100.net'
+
 def main():
-    for iata in table:
-        for i in range(1, 40):
-            for j in range(1, 90):
-                print iata + str(i).zfill(2) + 's' + str(j).zfill(2) + '-in-'\
-                      'x' + '01.1e100.net'
+    if len(sys.argv) > 1:
+        list_1e100(sys.argv[1])
+    else:
+        for iata in table:
+            list_1e100(iata)
 
 if __name__ == '__main__':
     main()

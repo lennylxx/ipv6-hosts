10ca1367e53eb42aafa4e8c62166d24ec8cd579d
gaoyuzhe22 <33866506+gaoyuzhe22@users.noreply.github.com>
2018-11-01 14:10:16 +0800

    Add files via upload
    针对win和unix平台下换行符的纠正

--- ipv6hostscomment.py
+++ ipv6hostscomment.py
@@ -1,9 +1,9 @@
 import re
 iplist={}
-addr=r'C:\Users\gyz\Desktop\hosts.out'  # The path of input log file
-#addr=r'C:\Users\gyz\Desktop\rawipv6hosts'  # The path of input log file
-addr2=r'C:\Users\gyz\Desktop\hosts.out1'
-with open(addr2, 'wt') as f: #写入的地址要改
+addr=r'hosts.out'  # The path of input log file
+addr2=r'hosts_hk'
+with open(addr2, 'wt',newline='\n') as f: #写入的地址要改
+	print('127.0.0.1 localhost \n::1 localhost ip6-localhost ip6-loopback',end='\n',file=f)
 	for raw in open(addr,encoding='UTF-8'):
 		if(re.search(r'scholar',raw)):
 			gyz=re.match(r'^\#',raw)

f3107e9c7580b4b8eed316cdbcd3c8c989040847
sharuijinfriend <33866506+sharuijinfriend@users.noreply.github.com>
2018-11-15 13:01:09 +0800

    Add files via upload

--- ipv6hostscomment.py
+++ ipv6hostscomment.py
@@ -21,14 +21,20 @@ while(conti):
 	else:
 		print('Something is wrong! Please input type of OS again')
 
+# with open(addr2, 'wt',newline=ss) as f: #写入的地址要改
+	# print('127.0.0.1 localhost \n::1 localhost ip6-localhost ip6-loopback',end='\n',file=f)
+	# for raw in open(addr,encoding='UTF-8'):
+		# if(re.search(r'scholar',raw)):
+			# gyz=re.match(r'^\#',raw)
+			# if(not gyz):
+				# print(raw,end='',file=f)
+			# else:
+				# print(re.match(r'^(\#)(.*)',raw).group(2),end='\n',file=f)
+		# elif((not re.match(r'^\#',raw)) & (not re.search(r'\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3}',raw)) & (not re.match(r'^\s+',raw)) ):
+			# print(raw,end='',file=f)
+			
 with open(addr2, 'wt',newline=ss) as f: #写入的地址要改
-	print('127.0.0.1 localhost \n::1 localhost ip6-localhost ip6-loopback',end='\n',file=f)
 	for raw in open(addr,encoding='UTF-8'):
-		if(re.search(r'scholar',raw)):
-			gyz=re.match(r'^\#',raw)
-			if(not gyz):
-				print(raw,end='',file=f)
-			else:
-				print(re.match(r'^(\#)(.*)',raw).group(2),end='\n',file=f)
-		elif((not re.match(r'^\#',raw)) & (not re.search(r'\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3}',raw)) & (not re.match(r'^\s+',raw)) ):
+		if((not re.match(r'^\#',raw)) & (not re.search(r'\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3}',raw)) & (not re.match(r'^\s+',raw)) ):
 			print(raw,end='',file=f)
+	print('127.0.0.1 localhost \n::1 localhost ip6-localhost ip6-loopback \n2404:6800:4005:800::2000 scholar.google.com \n2404:6800:4005:800::2003 scholar.google.com.hk \n2404:6800:4005:800::2003 scholar.google.com.tw \n2404:6800:4005:800::2000 android.clients.google.com \n2404:6800:4005:800::2000 console.developer.google.com\n2404:6800:4008:c06::52 console.developers.google.com\n2404:6800:4008:c06::7b wifi.google.com\n2404:6800:4005:800::2000 www.google.org\n2404:6800:4005:800::2000 www.chromium.org\n2404:6800:4005:800::2000 dev.chromium.org\n2404:6800:4005:800::2000 blog.chromium.org\n2404:6800:4005:800::2000 android.l.google.com\n2404:6800:4005:800::2000 scholar.l.google.com\n2404:6800:4008:c06::7b wifi.l.google.com',end='\n',file=f)

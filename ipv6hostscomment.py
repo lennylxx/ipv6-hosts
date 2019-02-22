81306894899edb65404539ea8099d27b2866f934
sharuijinfriend <33866506+sharuijinfriend@users.noreply.github.com>
2019-02-22 20:23:15 +0800

    Add files via upload

--- ipv6hostscomment.py
+++ ipv6hostscomment.py
@@ -1,6 +1,6 @@
 import re
 iplist={}
-addr=r'hosts.out'  # The path of input log file
+addr=r'hosts_hk.out'  # The path of input log file
 
 
 conti=1
@@ -37,6 +37,6 @@ with open(addr2, 'wt',newline=ss) as f: #写入的地址要改
 	for raw in open(addr,encoding='UTF-8'):
 		if((not re.match(r'^\#',raw)) & (not re.search(r'\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3}',raw)) & (not re.match(r'^\s+',raw)) ):
 			print(raw,end='',file=f)
-	print('127.0.0.1 localhost \n::1 localhost ip6-localhost ip6-loopback \n2404:6800:4005:800::2000 scholar.google.com \n2404:6800:4005:800::2003 scholar.google.com.hk \n2404:6800:4005:800::2003 scholar.google.com.tw \n2404:6800:4005:800::2000 android.clients.google.com \n2404:6800:4005:800::2000 console.developer.google.com\n2404:6800:4008:c06::52 console.developers.google.com\n2404:6800:4008:c06::7b wifi.google.com\n2404:6800:4005:800::2000 www.google.org\n2404:6800:4005:800::2000 www.chromium.org\n2404:6800:4005:800::2000 dev.chromium.org\n2404:6800:4005:800::2000 blog.chromium.org\n2404:6800:4005:800::2000 android.l.google.com\n2404:6800:4005:800::2000 scholar.l.google.com\n2404:6800:4008:c06::7b wifi.l.google.com',end='\n',file=f)
-	if(choose=='Unix'):
-		print('2400:da00:2::29 www.baidu.com',end='\n',file=f) #accelerate baidu speed in qujiang
+	print('127.0.0.1 localhost \n::1 localhost ip6-localhost ip6-loopback \n2404:6800:4005:800::2000 scholar.google.com \n2404:6800:4005:800::2000 scholar.google.com.hk \n2404:6800:4005:800::2000 scholar.google.com.tw \n2404:6800:4005:800::2000 android.clients.google.com \n2404:6800:4005:800::2000 console.developer.google.com\n2404:6800:4008:c06::52 console.developers.google.com\n2404:6800:4008:c06::7b wifi.google.com\n2404:6800:4005:800::2000 www.google.org\n2404:6800:4005:800::2000 www.chromium.org\n2404:6800:4005:800::2000 dev.chromium.org\n2404:6800:4005:800::2000 blog.chromium.org\n2404:6800:4005:800::2000 android.l.google.com\n2404:6800:4005:800::2000 scholar.l.google.com\n2404:6800:4008:c06::7b wifi.l.google.com',end='\n',file=f)
+	# if(choose=='Unix'):
+# print('2400:da00:2::29 www.baidu.com',end='\n',file=f) #accelerate baidu speed 
